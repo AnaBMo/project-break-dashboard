1862505e15e6bca8e496eb03fd5cf51d892ead0a
AnaBMo <anab_mj@outlook.com>
2024-12-14 19:14:54 +0100

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ function iniciarCambioDeFondo() {
     let indiceImagen = Math.floor(Math.random() * 10) + 1; // comenzar con una imagen aleatoria 
 
     // configuración DOM para que arranque con la primera imagen aleatoria:
-    document.body.style.backgroundImage = `url(./assets/img/playa${indiceImagen}.jpg)`;
+    document.body.style.backgroundImage = `url(/assets/img/playa${indiceImagen}.jpg)`;
 
     // cambiar de imagen cada 10 segundos incrementando el indice
     setInterval(() => {
         indiceImagen = indiceImagen < 10 ? indiceImagen + 1 : 1; 
-        document.body.style.backgroundImage = `url(./assets/img/playa${indiceImagen}.jpg)`;
+        document.body.style.backgroundImage = `url(/assets/img/playa${indiceImagen}.jpg)`;
     }, 10000);
 }
 
