4a2a8cf411c414f8f2a71b47400599c30c9349ea
AnaBMo <anab_mj@outlook.com>
2024-12-14 20:24:34 +0100

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ Se muestra una imagen de fondo que va cambiando cada 10 segundos.
 Se crea la variable "contador" con número de imagen para poder aumentarlo y así 
 recorrer las imágenes de la carpeta añadiendo el dato a la ruta.
 ********************************************************************************** */
+function iniciarCambioDeFondoIndex() {
+
     let indiceImagen = Math.floor(Math.random() * 10) + 1; // comenzar con una imagen aleatoria 
 
     // configuración DOM para que arranque con la primera imagen aleatoria:
@@ -14,3 +16,8 @@ recorrer las imágenes de la carpeta añadiendo el dato a la ruta.
         indiceImagen = indiceImagen < 10 ? indiceImagen + 1 : 1; 
         document.body.style.backgroundImage = `url(./assets/img/playa${indiceImagen}.jpg)`;
     }, 10000);
+}
+
+
+// cargar las imágenes de fondo
+iniciarCambioDeFondoIndex()
