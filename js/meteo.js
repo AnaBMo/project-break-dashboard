55d01acf4c83c832d873fdd1f36c042ca2b29f82
AnaBMo <anab_mj@outlook.com>
2024-12-15 12:59:41 +0100

    12:59

--- js/meteo.js
+++ js/meteo.js
@@ -4,23 +4,8 @@ Se muestra una imagen de fondo que va cambiando cada 10 segundos.
 Se crea la variable "contador" con número de imagen para poder aumentarlo y así 
 recorrer las imágenes de la carpeta añadiendo el dato a la ruta.
 ********************************************************************************** */
-function iniciarCambioDeFondo() {
 
-    let indiceImagen = Math.floor(Math.random() * 10) + 1; // comenzar con una imagen aleatoria 
-
-    // configuración DOM para que arranque con la primera imagen aleatoria:
-    document.body.style.backgroundImage = `url(../assets/img/playa${indiceImagen}.jpg)`;
-
-    // cambiar de imagen cada 10 segundos incrementando el indice
-    setInterval(() => {
-        indiceImagen = indiceImagen < 10 ? indiceImagen + 1 : 1; 
-        document.body.style.backgroundImage = `url(../assets/img/playa${indiceImagen}.jpg)`;
-    }, 10000);
-}
-
-
-// cargar las imágenes de fondo
-iniciarCambioDeFondo()
+iniciarCambioDeFondo(quitarPunto=false);
 
 /* ***********************************************************************
 CONSULTAR EL TIEMPO
